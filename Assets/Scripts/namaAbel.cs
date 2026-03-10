cf3bc0649af863c28724afaae06aaa86d6a0e582
Lixioo991 <tiktokgabut777@gmail.com>
2026-03-10 10:48:30 +0700

    Pasan yang saya masukan penambahan nama Abel

---
+++ Assets/Scripts/namaAbel.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class namaAbel : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello, Abel!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
