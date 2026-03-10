8d77d6d70bebb1e459dc7061c91c306292443568
Lixioo991 <tiktokgabut777@gmail.com>
2026-03-10 11:14:22 +0700

    Abel Sedang Memasukan Nama

--- Assets/Scripts/namaAbel.cs
+++ Assets/Scripts/namaAbel.cs
@@ -5,7 +5,7 @@ public class namaAbel : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello, Abel!");
+        Debug.Log("Hello, Abellian");
     }
 
     // Update is called once per frame
